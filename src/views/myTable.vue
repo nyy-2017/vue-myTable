74c1fbf5c8ea3cdc7d2cd151e6b32a0f11fef8ff
nieyangyang712 <290385211@qq.com>
2022-05-04 23:54:24 +0800

    add modal

--- src/views/myTable.vue
+++ src/views/myTable.vue
@@ -3,49 +3,124 @@
     <h2>{{tableSettings.title}}</h2>
     <v-table @select="handleSelect" :data="tableData" :header="tableHeader" :settings="tableSettings" />
     <h3> 选中的值：{{ selecteds}} </h3>
+    <!-- 子组件 -->
+    <myTable-modal ref="myTableModal1" />
   </div>
 </template>
 
 <script>
 import vTable from "@/components/ui-table/table.vue";
+import myTableModal from './mytableModal.vue'
 import constant from "@/components/constant.js";
 export default {
-  name: "myTable",
-  data() {
-    return {
-      tableData: [],
-      tableHeade: [],
-      tableSettings: {},
-      selecteds: []
-    };
-  },
-  components: { vTable },
-  methods: {
-    handleSelect(type, e) {
-      console.log("e:", type, e);
-      // this.selecteds = e
+    name: "myTable",
+    data() {
+        return {
+        tableData: [],
+        tableHeade: [],
+        tableSettings: {},
+        selecteds: []
+        };
     },
-  },
-  created() {
-    this.tableHeader = constant.tableHeader;
-    this.tableSettings = constant.tableSettings;
-    for (let i = 0; i < 30; i++) {
-      this.tableData.push({
-        name: "王小虎111",
-        department: "财务部",
-        workCode: "SPT0108",
-        annualLeave: 10,
-        privateAffairLeave: 1,
-        sickLeave: 1,
-        maternityLeave: 1,
-        paternityLeave: 1,
-        doubleOvertime: 100,
-        threeOvertime: 1000,
-        totalAllowance: 12222,
-        attendanceCoefficient: 12,
-        signature: "王小虎"
-      });
+    components: { vTable, myTableModal },
+    methods: {
+        handleSelect(type, e) {
+            console.log("e:", type, e);
+            if (type == 'select'){
+                this.selecteds = e
+            } else if (type == 'warning') {
+                this.$refs.myTableModal1.modal1 = true;
+                this.$refs.myTableModal1.titles = '查看详情';
+            } else if (type == 'edit') {
+                this.$refs.myTableModal1.modal1 = true;
+                this.$refs.myTableModal1.titles = '修改';
+                this.$refs.myTableModal1.fromData = e
+            }
+        },
+    },
+    created() {
+        this.tableHeader = constant.tableHeader;
+        this.tableSettings = constant.tableSettings;
+
+        this.tableData = [
+            {
+                name: "王小虎111",
+                department: "财务部",
+                workCode: "SPT0108",
+                annualLeave: 10,
+                privateAffairLeave: 1,
+                sickLeave: 1,
+                maternityLeave: 1,
+                paternityLeave: 1,
+                doubleOvertime: 100,
+                threeOvertime: 1000,
+                totalAllowance: 12222,
+                attendanceCoefficient: 12,
+                signature: "王小虎1"
+            },
+            {
+                name: "王小虎222",
+                department: "财务部",
+                workCode: "SPT0109",
+                annualLeave: 10,
+                privateAffairLeave: 1,
+                sickLeave: 1,
+                maternityLeave: 1,
+                paternityLeave: 1,
+                doubleOvertime: 100,
+                threeOvertime: 1000,
+                totalAllowance: 12222,
+                attendanceCoefficient: 12,
+                signature: "王小虎2"
+            },
+            {
+                name: "王小虎333",
+                department: "财务部",
+                workCode: "SPT0110",
+                annualLeave: 10,
+                privateAffairLeave: 1,
+                sickLeave: 1,
+                maternityLeave: 1,
+                paternityLeave: 1,
+                doubleOvertime: 100,
+                threeOvertime: 1000,
+                totalAllowance: 12222,
+                attendanceCoefficient: 12,
+                signature: "王小虎3"
+            },
+            {
+                name: "王小虎444",
+                department: "财务部",
+                workCode: "SPT0111",
+                annualLeave: 10,
+                privateAffairLeave: 1,
+                sickLeave: 1,
+                maternityLeave: 1,
+                paternityLeave: 1,
+                doubleOvertime: 100,
+                threeOvertime: 1000,
+                totalAllowance: 12222,
+                attendanceCoefficient: 12,
+                signature: "王小虎4"
+            },
+            {
+                name: "王小虎555",
+                department: "财务部",
+                workCode: "SPT0112",
+                annualLeave: 10,
+                privateAffairLeave: 1,
+                sickLeave: 1,
+                maternityLeave: 1,
+                paternityLeave: 1,
+                doubleOvertime: 100,
+                threeOvertime: 1000,
+                totalAllowance: 12222,
+                attendanceCoefficient: 12,
+                signature: "王小虎5"
+            }
+
+        ]
+      
     }
-  }
-};
+}
 </script>
---
+++ src/views/mytableModal.vue
@@ -0,0 +1,126 @@
+<template>
+    <el-dialog :title="titles" :visible.sync="modal1" width="60%" :before-close="handleClose">
+        <el-form :model="fromData" :rules="rules" ref="fromData" label-width="100px">
+            <el-row>
+                <el-col :span="12">
+                    <el-form-item label="姓名" prop="name">
+                        <el-input v-model="fromData.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="部门" prop="department">
+                        <el-input v-model="fromData.department"></el-input>
+                    </el-form-item>
+                    <el-form-item label="工号" prop="workCode">
+                        <el-input v-model="fromData.workCode"></el-input>
+                    </el-form-item>
+                    <el-form-item label="年假(天)" prop="annualLeave">
+                        <el-input v-model="fromData.annualLeave"></el-input>
+                    </el-form-item>
+                    <el-form-item label="事假(小时)" prop="privateAffairLeave">
+                        <el-input v-model="fromData.privateAffairLeave"></el-input>
+                    </el-form-item>
+                    <el-form-item label="病假(小时)" prop="sickLeave">
+                        <el-input v-model="fromData.sickLeave"></el-input>
+                    </el-form-item>
+                    <el-form-item label="产假(天)" prop="maternityLeave">
+                        <el-input v-model="fromData.maternityLeave"></el-input>
+                    </el-form-item>
+
+                </el-col>
+
+                <el-col :span="12">
+                    <el-form-item label="陪产假(小时)" prop="paternityLeave">
+                        <el-input v-model="fromData.paternityLeave"></el-input>
+                    </el-form-item>
+                    <el-form-item label="二倍加班" prop="doubleOvertime">
+                        <el-input v-model="fromData.doubleOvertime"></el-input>
+                    </el-form-item>
+                    <el-form-item label="三倍加班" prop="threeOvertime">
+                        <el-input v-model="fromData.threeOvertime"></el-input>
+                    </el-form-item>
+                    <el-form-item label="津贴合计" prop="totalAllowance">
+                        <el-input v-model="fromData.totalAllowance"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出勤系数" prop="attendanceCoefficient">
+                        <el-input v-model="fromData.attendanceCoefficient"></el-input>
+                    </el-form-item>
+                    <el-form-item label="签名" prop="signature">
+                        <el-input v-model="fromData.signature"></el-input>
+                    </el-form-item>
+
+                </el-col>
+            </el-row>
+
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('fromData')">立即创建</el-button>
+                <el-button @click="resetForm('fromData')">重置</el-button>
+            </el-form-item>
+        </el-form>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+    name: 'myTableModal',
+    data () {
+        return {
+            modal1: false,
+            titles: '',
+            fromData: {
+                name: '',
+                department: '',
+                workCode: '',
+                annualLeave: '',
+                maternityLeave: '',
+                paternityLeave: '',
+                doubleOvertime: '',
+                threeOvertime: '',
+                totalAllowance: '',
+                attendanceCoefficient: '',
+                signature: ""
+            },
+            rules: {
+                name: [
+                    { required: true, message: '请输入名称', trigger: 'blur' },
+                    { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                ],
+                // department: [
+                //     { required: true, message: '请选择活动区域', trigger: 'change' }
+                // ],
+                // date1: [
+                //     { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+                // ],
+                // date2: [
+                //     { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+                // ],
+                // type: [
+                //     { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+                // ],
+                // resource: [
+                //     { required: true, message: '请选择活动资源', trigger: 'change' }
+                // ],
+                // desc: [
+                //     { required: true, message: '请填写活动形式', trigger: 'blur' }
+                // ]
+            }
+        }
+    },
+    methods:{
+        handleClose() {
+            this.modal1 = false
+        },
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        }
+    }
+}
+</script>
